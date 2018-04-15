a6769b78f27e4ef0bdafca290cd6c72eefbbc9e7
robertboothby <bob.boothby@gmail.com>
2018-04-15 16:02:11 +0100

    Fully implemented basic mojo for generating Generators and GeneratorBuilders without defaults.

--- source-generator-mojo/src/main/java/com/robertboothby/djenni/DjenniGenerateSourcesMojo.java
+++ source-generator-mojo/src/main/java/com/robertboothby/djenni/DjenniGenerateSourcesMojo.java
@@ -1,10 +1,12 @@
 package com.robertboothby.djenni;
 
 import com.robertboothby.template.AbstractGeneratorMojo;
+import com.robertboothby.template.model.GenerationModel;
 import com.robertboothby.utilities.lambda.FunctionResult;
 import com.robertboothby.template.model.GenerationModelRetriever;
 import com.robertboothby.utilities.lambda.LambdaUtilities;
 import com.thoughtworks.qdox.JavaProjectBuilder;
+import com.thoughtworks.qdox.model.JavaClass;
 import com.thoughtworks.qdox.model.JavaSource;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
@@ -15,10 +17,14 @@ import org.apache.maven.shared.model.fileset.util.FileSetManager;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static com.robertboothby.utilities.lambda.LambdaUtilities.not;
 import static com.robertboothby.utilities.lambda.LambdaUtilities.wrap;
 import static java.util.Arrays.stream;
 
@@ -45,7 +51,7 @@ public class DjenniGenerateSourcesMojo extends AbstractGeneratorMojo {
 
         JavaProjectBuilder builder = new JavaProjectBuilder();
 
-        List<FunctionResult<JavaSource>> sourceFiles =
+        List<FunctionResult<JavaSource>> results =
                 stream(fileSets)
                 .flatMap(this::getIncludedFiles)
                 .collect(Collectors.toSet()) //Ensure uniqueness
@@ -54,7 +60,7 @@ public class DjenniGenerateSourcesMojo extends AbstractGeneratorMojo {
                 .collect(Collectors.toList());
 
         //Check for failures
-        String failures = sourceFiles.stream()
+        String failures = results.stream()
                 .filter(FunctionResult::isExceptional)
                 .map(FunctionResult::toString)
                 .collect(Collectors.joining("\n"));
@@ -63,49 +69,61 @@ public class DjenniGenerateSourcesMojo extends AbstractGeneratorMojo {
             throw new MojoExecutionException("Failed to parse files: \n" + failures);
         }
 
+        return () -> results
+                .stream()
+                .map(FunctionResult::getResult)
+                .map(Optional::get)
+                .flatMap(this::createGenerationModel)
+                .collect(Collectors.toList());
+    }
 
-        return null;
+    private Stream<GenerationModel> createGenerationModel(JavaSource javaSource) {
+        //Filter once so that we can reuse it.
+        List<JavaClass> filteredJavaClasses = javaSource.getClasses()
+                .stream()
+                .filter(not(JavaClass::isInterface))
+                .filter(not(JavaClass::isInner))
+                .filter(not(JavaClass::isAbstract))
+                .filter(not(JavaClass::isEnum))
+                .filter(JavaClass::isPublic)
+                .collect(Collectors.toList());
+
+        return Stream.concat(getGeneratorModels(javaSource, filteredJavaClasses), getGeneratorBuilderModels(javaSource, filteredJavaClasses));
+    }
+
+    private Stream<GenerationModel> getGeneratorBuilderModels(JavaSource javaSource, List<JavaClass> filteredJavaClasses) {
+        return filteredJavaClasses
+                .stream()
+                .map(javaClass ->
+                        new GenerationModel(
+                                "Generator.ftl",
+                                getModel(javaSource, javaClass),
+                                javaClass.getFullyQualifiedName().replace('.', '/') + "Generator.java"
+                        )
+                );
+    }
+
+    private Stream<GenerationModel> getGeneratorModels(JavaSource javaSource, List<JavaClass> filteredJavaClasses) {
+        return filteredJavaClasses
+                .stream()
+                .map(javaClass ->
+                        new GenerationModel(
+                                "GeneratorBuilder.ftl",
+                                getModel(javaSource, javaClass),
+                                javaClass.getFullyQualifiedName().replace('.', '/') + "GeneratorBuilder.java"
+                        )
+                );
+    }
+
+    private Map<String, Object> getModel(JavaSource javaSource, JavaClass javaClass) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("javaSource", javaSource);
+        result.put("javaClass", javaClass);
+        return result;
     }
 
     private Stream<File> getIncludedFiles(FileSet fileSet) {
         return stream(fileSetManager.getIncludedFiles(fileSet))
                 .map(file -> new File(new File(fileSet.getDirectory()), file));
     }
-
-
-//    public void execute() throws MojoExecutionException {
-//
-//        SourceCodeGenerator sourceCodeGenerator = new SourceCodeGenerator();
-//        final File destinationRoot = isGeneratingTestSources ? testSourceDestination : sourceDestination;
-//        sourceCodeGenerator.setDestinationRoot(destinationRoot);
-//
-//        getLog().info("Generating Djenni Sources");
-//
-//        try{
-//            getLog().info("Destination : " + destinationRoot.getCanonicalPath());
-//        } catch (IOException e) {
-//            throw new MojoExecutionException("Could not resolve destination directory", e);
-//        }
-//
-//        DirectoryScanner scanner = new DirectoryScanner();
-//        scanner.setIncludes(includes);
-//        scanner.setExcludes(excludes);
-//        for (File sourceRoot : sourceRoots){
-//            scanner.setBasedir(sourceRoot);
-//            scanner.scan();
-//            String[] files = scanner.getIncludedFiles();
-//            try {
-//                sourceCodeGenerator.generateSourceFiles(true, generateForCollectionGetters, sourceRoot, files);
-//            } catch (IOException e) {
-//                throw new MojoExecutionException("Problem with processing a source root : " + sourceRoot, e);
-//            }
-//        }
-//
-//        if(isGeneratingTestSources){
-//            project.addTestCompileSourceRoot(destinationRoot.getAbsolutePath());
-//        } else {
-//            project.addCompileSourceRoot(destinationRoot.getAbsolutePath());
-//        }
-//
-//    }
 }
